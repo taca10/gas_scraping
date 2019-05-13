c2d55c1673b2caa57fcb51c511e1cb4749058781
taca10 <taka.ur.0505@gmail.com>
2019-05-13 16:04:58 +0900

    scraping練習

---
+++ scraping.js
@@ -0,0 +1,20 @@
+function scraping() {
+  var blog_url_array = '';
+  var topUrl = 'http://at-jinji.jp/blog/all/';
+  var sheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sht = sheet.getSheetByName('シート1');
+  var links = [];
+     
+  for (i=1; i<51; i++) {
+    var getUrl = "http://at-jinji.jp/blog/all/page/" + i;
+    var content = UrlFetchApp.fetch(getUrl).getContentText('UTF-8');
+    var itemRegexp = new RegExp(/https:\/\/at-jinji.jp\/blog\/[0-9]+/g);
+    var blogUrlArray = content.match(itemRegexp);
+    for(j=0; j<blogUrlArray.length; j++) {
+      links.push([blogUrlArray[j]]);
+    }
+  }
+  
+  sht.getRange(2, 2, links.length, 1).setValues(links);
+  
+}
